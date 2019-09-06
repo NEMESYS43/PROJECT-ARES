09dad29b351d27632c7a628d6c34d4364a083e91
Nik Hendricks <34929372+NEMESYS43@users.noreply.github.com>
2019-09-05 20:25:10 -0500

    Add files via upload

--- adminPanel/js/connection.js
+++ adminPanel/js/connection.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:4343");
+var socket = io.connect("http://104.237.111.55:4343/");
 socket.on("connection", function(socket) {
   console.log("ESTABLISHED CONNECTION WITH MASTER SERVER WITH SOCKET ID " + socket);
 });
